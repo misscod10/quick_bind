4e9bb66fdb08e6804a472e4e13637e7abf26c016
misscod10 <misscod10@gmail.com>
2022-10-26 09:15:24 +0200

    arreglado

--- install_server.sh
+++ install_server.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade
 sudo apt install vim unzip bind9 bind9utils bind9-doc isc-dhcp-server netplan
 cd /etc/
-sudo unzip ~/quick_bind/zips/bind.zip 
-sudo unzip ~/quick_bind/zips/dhcp.zip 
+sudo unzip ~/quick_bind/zips/bind.zip /etc 
+sudo unzip ~/quick_bind/zips/dhcp.zip /etc
 sudo cp ~/quick_bind/netplan_server/00-installer-config-yaml ./netplan/
 sudo netplan apply
